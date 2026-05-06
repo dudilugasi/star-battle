69c14d38501edfcc8b95aae4aaf07e22d2873ca8
dudilugasi <dulu123@gmail.com>
2026-05-06 23:27:54 +0300

    added puzzles

--- lib/puzzles/index.ts
+++ lib/puzzles/index.ts
@@ -1,14 +1,8 @@
 import { Puzzle } from "@/lib/types";
-import { puzzleE1_5x5 } from "./e1-5x5";
-import { puzzleM1_5x5 } from "./m1-5x5";
-import { puzzleM1_8x8 } from "./m1-8x8";
-import { puzzleH1_8x8 } from "./h1-8x8";
+import { puzzles as puzzles1x1 } from "./1_1";
 
 export const PUZZLES: Puzzle[] = [
-  puzzleE1_5x5,
-  puzzleM1_5x5,
-  puzzleM1_8x8,
-  puzzleH1_8x8,
+    ...puzzles1x1
 ];
 
 export function findPuzzle(id: string): Puzzle | undefined {
